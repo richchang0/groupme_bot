765406acbeda58064c9902645d28e224569e10d6
Rich Chang <rchang@signal.co>
2017-04-12 14:10:41 -0500

    startup message

--- clock.py
+++ clock.py
@@ -1,8 +1,9 @@
-from groupme import send_daily_message_count, tell_frank_to_go_home
+from groupme import send_daily_message_count, tell_frank_to_go_home, send_message_to_group
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 
 if __name__ == '__main__':
+  send_message_to_group("i'm back bois")
   sched = BlockingScheduler()
   sched.add_job(send_daily_message_count, 'cron', hour=23, minute=59, timezone='America/Chicago')
   sched.add_job(tell_frank_to_go_home, 'cron', hour=6, day_of_week='mon-fri', timezone='America/Los_Angeles')
