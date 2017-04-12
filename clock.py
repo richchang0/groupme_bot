027f5a30a9e3bca98e533e392cb5bbbe891f14db
Rich Chang <rchang@signal.co>
2017-04-12 14:17:52 -0500

    fix startup msg

--- clock.py
+++ clock.py
@@ -1,9 +1,9 @@
-from groupme import send_daily_message_count, tell_frank_to_go_home, send_message_to_group
+from groupme import send_daily_message_count, tell_frank_to_go_home, startup_message
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 
 if __name__ == '__main__':
-  send_message_to_group("i'm back bois")
+  startup_message()
   sched = BlockingScheduler()
   sched.add_job(send_daily_message_count, 'cron', hour=23, minute=59, timezone='America/Chicago')
   sched.add_job(tell_frank_to_go_home, 'cron', hour=6, day_of_week='mon-fri', timezone='America/Los_Angeles')
